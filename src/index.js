73eb71e3bf33c0390a6b8d6c905e27064e68584c
Jenmaru <1vanpersonstudio@gmail.com>
2023-09-16 15:29:32 +0300

    step6

--- src/index.js
+++ src/index.js
@@ -12,27 +12,26 @@ const getCleanString = (dirtResult) => {
     return getCleanString(`${dirtResult.substring(0, cleanResult - 7)}${dirtResult.substring(cleanResult +3, dirtResult.length)}`)
   }
 
-const buildTree = (diffFile1, diffFile2, count = 0) => {
-    count += 1;
+const buildTree = (diffFile1, diffFile2) => {
     const fileKeys = _.union(_.keys(diffFile1), _.keys(diffFile2));
     const mapKeys = fileKeys.map(key => {
         const oldValue = diffFile1[key];
         const newValue = diffFile2[key];
         if (!_.has(diffFile2, key)) {
-            return {key, state: 'deleted', value: oldValue, level: count};
+            return {key, state: 'deleted', value: oldValue};
         }
         if (!_.has(diffFile1, key)) {
-            return {key, state: 'added', value: newValue, level: count};
+            return {key, state: 'added', value: newValue};
         }
         if (_.has(diffFile1, key) && _.has(diffFile2, key)) {
             if (_.isObject(oldValue) && _.isObject(newValue)) {
-                return {key, state: 'merge', children: buildTree(oldValue, newValue, count), level: count};
+                return {key, state: 'merge', children: buildTree(oldValue, newValue)};
             }
             if (oldValue === newValue) {
-                return {key, state: 'unchanged', value: oldValue, level: count};
+                return {key, state: 'unchanged', value: oldValue};
             }
         }
-        return {key, state: 'changed', oldValue, newValue, level: count};
+        return {key, state: 'changed', oldValue, newValue};
     });
     return mapKeys;
 };
