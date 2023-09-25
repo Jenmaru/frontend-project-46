7bd3a6b900409fcf842d28fefefef00cc6a32214
Jenmaru <1vanpersonstudio@gmail.com>
2023-09-25 16:46:11 +0300

    step8

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -6,16 +6,12 @@ const extention = ['json'];
 
 describe('gendiff', () => {
   const recursiveResult = fs.readFileSync(path.resolve('./__tests__/fixtures/result'), 'utf8');
-  const plainResult = fs.readFileSync(path.resolve('./__tests__/fixtures/plain'), 'utf8');
-  const jsonResult = fs.readFileSync(path.resolve('./__tests__/fixtures/json'), 'utf8');
 
-  describe.each(extention)('compare two %s files', (ext) => {
     const before = path.resolve(`./__tests__/fixtures/before.json`);
     const after = path.resolve(`./__tests__/fixtures/after.json`);
-    const expected = genDiff(after, before);
+    const expected = genDiff(after, before, 'stylish');
 
     test('stylish', () => {
       expect(expected).toEqual(recursiveResult);
     });
   });
-});
