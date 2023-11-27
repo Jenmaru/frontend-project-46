25b81fe981c06a52e9582db9c7207362b28d4072
Jenmaru <1vanpersonstudio@gmail.com>
2023-11-27 17:01:08 +0300

    refactor3

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -3,9 +3,7 @@ import fs from 'fs';
 import { fileURLToPath } from 'node:url';
 import genDiff from '../src/index.js';
 
-// eslint-disable-next-line no-underscore-dangle
 const __filename = fileURLToPath(import.meta.url);
-// eslint-disable-next-line no-underscore-dangle
 const __dirname = path.dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
